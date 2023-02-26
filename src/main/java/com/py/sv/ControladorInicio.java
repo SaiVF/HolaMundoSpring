b6775c231c09851916de7bb89e9f55ca299089af
Santiago Vargas <sai19.sv@gmail.com>
2023-02-26 13:39:33 -0300

    Compartir información con Spring MVC y Thymeleaf

--- src/main/java/com/py/sv/ControladorInicio.java
+++ src/main/java/com/py/sv/ControladorInicio.java
@@ -2,14 +2,21 @@ package com.py.sv;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+    @Value("${index.saludo}")
+    private String saludo;
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        var mensaje = "Hola mundo con Thymeleaf";
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo",saludo);
         log.info("ejecutando el controlador Spring MVC");
         return "index";
     }
